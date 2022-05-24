39143e5a4d35340180f804aaac33e1cc97328b89
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-05-23 23:24:19 -0300

    Primeiro commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+    String name = "Vinícius Gabriel";
+    print("Olá, ${name}! Bem-vindo ao mundo Dart.");
+
+    int value = 05;
+    print(value);
+
+    bool variable = true;
+    print(variable);
+
+    List<String> listValues = ["Vinícius", "Gabriel"];
+    print(listValues);
+}
