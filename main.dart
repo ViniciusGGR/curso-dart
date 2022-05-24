e2cffa4e85c1232fe3267bfe3ff1060a8014338b
ViniciusGGR <vinicius.gmdigital@gmail.com>
2022-05-24 13:35:13 -0300

    if e switch

--- main.dart
+++ main.dart
@@ -28,4 +28,40 @@ void main() {
     late String nameSurname;
     nameSurname = "Vinícius";
     print(nameSurname);
+
+    // Estruturas de controle de fluxo:
+
+    bool moveOn = false;
+
+    if(moveOn) {
+      // Verifica se a condição é verdadeira.
+      print("Mova-se");
+    } else {
+      // Verifica se a condição é falsa.
+      print("Pare");
+    }
+
+    if(10 > 5) {
+      print("10 é maior que 5");
+    }
+
+    // Uso do Switch para printar um valor.
+    int valueInt = 5;
+
+    switch(valueInt) {
+      case 0:
+        print("ZERO");
+        break;
+      case 1:
+        print("UM");
+        break;
+      case 2:
+        print("DOIS");
+        break;
+
+      // Toda vez que os cases não relatarem a condição esperada do valor de entrada, será apresentado o valor default/padrão.
+        // Como o default é a última instrução não é necessário o break.
+      default:
+        print("PADRÃO");
+    }
 }
